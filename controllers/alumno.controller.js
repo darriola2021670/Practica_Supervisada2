860b8172cb5ffeb42bf0d8211189359fe29cc309
darriola-2021670 <darriola-2021670@kinal.edu.gt>
2024-02-13 15:56:49 -0600

    se hizo controller alumno y se agrego el post

---
+++ controllers/alumno.controller.js
@@ -0,0 +1,20 @@
+const bcryptsjs = require('bcryptsjs');
+const Alumno = require('../models/alumno');
+const {response, request} = require('express');
+
+const AlumnosPost = async (req, res) =>{
+    const {nombre, correo, password, carne, cursos, role} = req.body;
+    const alumno = new Alumno({nombre, correo, password, carne, cursos, role});
+
+    const salt = bcryptsjs.genSaltSync();
+    alumno.password = bcryptsjs.hashSync(password, salt);
+
+    await usuario.save();
+    res.status(202).json({
+        alumno
+    });
+}
+
+module.exports = {
+    AlumnosPost
+}
