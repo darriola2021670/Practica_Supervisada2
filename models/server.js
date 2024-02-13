7c67928391dddfadf4173edd8a20f5d4511d1fe4
darriola-2021670 <darriola-2021670@kinal.edu.gt>
2024-02-13 16:48:50 -0600

    se hizo merege feature/controllers

--- models/server.js
+++ models/server.js
@@ -6,6 +6,8 @@ class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
+        this.alumnoPath = '/api/alumnos';
+        this.profesorPath = '/api/profesores';
 
         this.conectarDB();
         this.middlewares();
