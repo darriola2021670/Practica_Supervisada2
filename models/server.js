ac56a2bdaa93b0f68ad059a3f046acf9ebb90519
darriola-2021670 <darriola-2021670@kinal.edu.gt>
2024-02-13 14:06:06 -0600

    se agrego codigo al index y server

--- models/server.js
+++ models/server.js
@@ -5,7 +5,30 @@ const { dbConnection } = require('../db/config');
 class Server{
     constructor(){
         this.app = express();
-        this.port = process.env.POTT
+        this.port = process.env.PORT;
+
+        this.conectarDB();
+        this.middlewares();
+        this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes(){
+    }
+
+    listen(){
+        this.app.listen(this.port, () =>{
+            console.log('Servidor ejecutandose y escuchando el puerto', this.port)
+        });
     }
 }
 
